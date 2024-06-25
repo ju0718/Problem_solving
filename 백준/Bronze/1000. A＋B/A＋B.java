8514a994a88723189e3c28d2fc602d7172a9cfd9
ju0718 <chaeju0718@naver.com>
2024-06-25 23:09:23 +0900

    [Bronze V] Title: A+B, Time: 184 ms, Memory: 17720 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int A=1, B=2;
+        A=sc.nextInt();
+        B=sc.nextInt();
+        
+        System.out.println(A+B);
+    }
+}
